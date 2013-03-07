2fdd582ca450d2dd9a7b1a775dd1d2a0eddd39d1
Mitchell Herrijgers <Mitchellherrijgers@gmail.com>
2013-03-07 11:17:38 +0100

    Loggin in possible

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+require 'user.php';
+
+if(isset($_SESSION['user-id'])) {
+	header('location: logboek.php');
+	exit;
+} else if (isset($_POST['email'])) {
+	if(authenticate($_POST['email'], $_POST['password']) != null) {
+		$user = current_user();
+		$_SESSION['user-id'] = $user['id'];
+		header('location: logboek.php');
+		exit;
+	} else {
+		echo "You entered the wrong account details, please try again! <br/><br/>";
+		showLoginForm();
+	}
+} else {
+	showLoginForm();
+}
+
+function showLoginForm()
+{
+	?>
+	<form method='POST'>
+		<label for='email'> E-mail: </label>
+		<input type='text' name='email' id='email'><br/>
+		<label for='password'> password: </label>
+		<input type='password' name='password' id='password'><br/>
+		<button type='submit'>log in!</button>
+	</form>
+	<br/><br/>
+	<a href='register.php'>or register here!</a>
+	<?php
+}
+?>
